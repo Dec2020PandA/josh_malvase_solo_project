2044716bbf086709b001a4f6aaacc2dcbeb1cf22
Josh Malvase <malvasej@gmail.com>
2020-12-08 21:42:31 -0800

    first commit

---
+++ bottle_shop/bottle_shop_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BottleShopAppConfig(AppConfig):
+    name = 'bottle_shop_app'
---
+++ bottle_shop/bottle_shop_app/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render, redirect
+
+def log_reg(request):
+    return render(request, "log_reg.html")
+
+
